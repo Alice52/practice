09ec081846e6f1a57c4d6af4e34a4726a775d66c
Alice52 <zzhang_xz@163.com>
2021-06-08 20:42:25 +0800

    [image]project/backend: add common actuator client and common actuator server

---
+++ backend/project-custom/actuator-server/src/main/java/common/actuator/server/ActuatorClientApplication.java
@@ -0,0 +1,19 @@
+package common.actuator.server;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zack <br>
+ * @create 2021-06-08<br>
+ * @project project-custom <br>
+ */
+@EnableAdminServer
+@SpringBootApplication
+public class ActuatorClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorClientApplication.class, args);
+    }
+}
