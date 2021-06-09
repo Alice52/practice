d0bab64a6d7c982a288548805251fc3827770d86
zack.zhang <zackzhang@augmentum.com.cn>
2021-06-09 13:26:36 +0800

    project/backend: refine actuator url and access log request timing

--- backend/project-custom/custom-test/src/main/java/custom/CustomApplication.java
+++ backend/project-custom/custom-test/src/main/java/custom/CustomApplication.java
@@ -1,8 +1,14 @@
 package custom;
 
 import common.swagger.annotation.EnableSwagger;
+import io.micrometer.core.instrument.util.StringUtils;
+import io.undertow.UndertowOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.web.embedded.undertow.ConfigurableUndertowWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.stereotype.Component;
 
 /**
  * @author zack <br>
@@ -15,5 +21,51 @@ public class CustomApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CustomApplication.class, args);
+    }
+
+    @Component
+    class UndertowWebServerAccessLogTimingEnabler
+            implements WebServerFactoryCustomizer<ConfigurableUndertowWebServerFactory> {
+
+        private final ServerProperties serverProperties;
+
+        public UndertowWebServerAccessLogTimingEnabler(ServerProperties serverProperties) {
+            this.serverProperties = serverProperties;
+        }
+
+        @Override
+        public void customize(ConfigurableUndertowWebServerFactory factory) {
+            String pattern = serverProperties.getUndertow().getAccesslog().getPattern();
+
+            // Record request timing only if the pattern
+            if (logRequestProcessingTiming(pattern)) {
+                factory.addBuilderCustomizers(
+                        builder ->
+                                builder.setServerOption(
+                                        UndertowOptions.RECORD_REQUEST_START_TIME, true));
+            }
+
+            // Custom access log receiver.
+            //            factory.addDeploymentInfoCustomizers(
+            //                    deploymentInfo -> {
+            //                        deploymentInfo.addInitialHandlerChainWrapper(
+            //                                handler -> {
+            //                                    Slf4jAccessLogReceiver accessLogReceiver =
+            //                                            new Slf4jAccessLogReceiver();
+            //                                    return new AccessLogHandler(
+            //                                            handler,
+            //                                            accessLogReceiver,
+            //                                            pattern,
+            //                                            Undertow.class.getClassLoader());
+            //                                });
+            //                    });
+        }
+
+        private boolean logRequestProcessingTiming(String pattern) {
+            if (StringUtils.isBlank(pattern)) {
+                return false;
+            }
+            return pattern.contains("%D") || pattern.contains("%T");
+        }
     }
 }
