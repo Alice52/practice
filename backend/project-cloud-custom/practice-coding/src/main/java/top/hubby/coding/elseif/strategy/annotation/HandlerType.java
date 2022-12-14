17a40cb6b7927ccd8513710ec77b2a6c3e5e87ba
alice52 <zzhang_xz@163.com>
2022-12-14 18:24:17 +0800

    backend/coding: add @see link for anno

--- backend/project-cloud-custom/practice-coding/src/main/java/top/hubby/coding/elseif/strategy/annotation/HandlerType.java
+++ backend/project-cloud-custom/practice-coding/src/main/java/top/hubby/coding/elseif/strategy/annotation/HandlerType.java
@@ -1,8 +1,11 @@
 package top.hubby.coding.elseif.strategy.annotation;
 
+import top.hubby.coding.elseif.strategy.component.OrderHandlerProcessor;
+
 import java.lang.annotation.*;
 
 /**
+ * @see OrderHandlerProcessor
  * @author zack <br>
  * @create 2020-12-28<br>
  * @project common-coding <br>
--- backend/project-cloud-custom/practice-coding/src/main/java/top/hubby/coding/elseif/strategy1/annotation/HandlerType.java
+++ backend/project-cloud-custom/practice-coding/src/main/java/top/hubby/coding/elseif/strategy1/annotation/HandlerType.java
@@ -1,8 +1,14 @@
 package top.hubby.coding.elseif.strategy1.annotation;
 
+import top.hubby.coding.elseif.strategy1.context.OrderHandlerContext;
+import top.hubby.coding.elseif.strategy1.handler.AbstractOrderHandler;
+
 import java.lang.annotation.*;
 
 /**
+ * @see HandlerTypeImpl
+ * @see OrderHandlerContext
+ * @see AbstractOrderHandler
  * @author zack <br>
  * @create 2020-12-28<br>
  * @project common-coding <br>
